de18db651d5adddecd55538b4ec990c128d88060
detonextsgames <victozingamer29@gmail.com>
2026-06-03 19:35:08 -0300

    Ver1.0.1

--- Projeto01/CalculadoraSimples.cs
+++ Projeto01/CalculadoraSimples.cs
@@ -1,11 +1,24 @@
 using System;
-namespace calculadora{
+namespace calculadora{ //Calculadora Simples C#
     internal class Program{
         static void Main(string[] args)
-        {
-            /////////Calculadora Simples Usando Entrada e Saída/////////////
+        { 
+            //var
+            int num1, num2, soma;
 
             Console.WriteLine("===----------CALCULADORA SIMPLES----------===");
+
+            Console.WriteLine("Digite um Numero: ");
+            num1 = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Digite outro Numero: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            //soma
+            soma = (num1 + num2);
+
+            //resultado
+            Console.WriteLine($"\nA Soma de {num1} + {num2} = " + soma);
 
         }
     }
