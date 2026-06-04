916ce921f331811e1ffd09f578e01817f71bfc8b
detonextsgames <victozingamer29@gmail.com>
2026-06-04 18:12:03 -0300

    Version 0.1.0

--- Projeto01/CalculadoraSimples.cs
+++ Projeto01/CalculadoraSimples.cs
@@ -4,7 +4,8 @@ namespace calculadora{ //Calculadora Simples C#
         static void Main(string[] args)
         { 
             //var
-            int num1, num2, soma;
+            int num1, num2;
+            float soma, sub, mult, div;
 
             Console.WriteLine("===----------CALCULADORA SIMPLES----------===");
 
@@ -14,12 +15,19 @@ namespace calculadora{ //Calculadora Simples C#
             Console.WriteLine("Digite outro Numero: ");
             num2 = int.Parse(Console.ReadLine());
 
-            //soma
+            //Resultado
             soma = (num1 + num2);
+            sub = (num1 - num2);
+            mult = (num1 * num2);
+            div = (num1 / num2);
 
             //resultado
             Console.WriteLine($"\nA Soma de {num1} + {num2} = " + soma);
+            Console.WriteLine($"\nA Subtração de {num1} - {num2} = " + sub);
+            Console.WriteLine($"\nA Multiplicação de {num1} * {num2} = " + mult);
+            Console.WriteLine($"\nA Divisão de {num1} / {num2} = " + div);
 
+            Console.WriteLine("\n------------------------------------------\n");
         }
     }
 }
